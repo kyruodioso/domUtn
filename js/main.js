9ebae58a0678b5eed55d4f728efd11130e3b5ea2
kyru odioso <kanu_nirvana@hotmail.com>
2021-07-16 03:24:04 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/*
+_Abrir el archivo evaluacion4.html para realizar el ejercicio y guardarlo como evaluacion4_apellido.html (por ejemplo: evaluacion4_perez.html)
+_Utilizando los contenidos desarrollados en esta unidad (DOM), realizar las siguientes acciones:
+*Aplicar color de fondo #fcf79f al documento.
+*Cambiar el color de todos los párrafos (etiqueta p) a verde (#0ca001).
+*Aumentar el tamaño de la fuente a 24px, solo del elemento con id "destacado".
+*Cambiar la familia tipográfica por Arial a los títulos (etiqueta h2).
+*Crear un vínculo con la referencia "https://www.lipsum.com".
+*/
+
+window.onload =()=>{
+//consignas
+    const fondo= document.body;
+    const parrafo= document.getElementsByTagName('p');
+    const destacado= document.getElementById('destacado');
+    
+cambiarFondo(fondo);
+cambiarColor(parrafo);
+cambiarFuente(destacado);
+crearEnlace();
+//Plus
+
+}
+
+const cambiarFondo =(element)=>{
+ element.style.backgroundColor="#fcf79f";
+}
+
+const cambiarColor=(element)=>{
+    for(let i=0; i<element.length; i++){
+        element[i].style.color="#0ca001";
+    }
+}
+
+const cambiarFuente=(element)=>{
+    for(let i=0; i<element.length; i++){
+        element[i].style.fontFamily="Arial"
+    }
+}
+
+const crearEnlace =(element)=>{
+    
+element=document.createElement("a");
+element.setAttribute("href","https://www.lipsum.com");
+const elementText = document.createTextNode("Lipsum");
+element.appendChild(elementText);
+document.body.appendChild(element);
+}
+
+
